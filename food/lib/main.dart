5608866910cdbdc518c707254b6ad367c9c13c4c
bisdev-pro <76934794+bisdev-pro@users.noreply.github.com>
2021-11-11 12:50:59 +0700

    lock Orientation

--- food/lib/main.dart
+++ food/lib/main.dart
@@ -5,8 +5,14 @@ import 'package:food/homepage.dart';
 import 'package:food/makanan/listMakanan.dart';
 import 'package:food/moor/reqMenu.dart';
 import 'package:food/splash.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
